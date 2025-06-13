81102b37ecb0d63416b75320d4f61e387b862467
AufaArib <aufa@maznusa.id>
2025-06-13 08:50:55 +0700

    api_url edit

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@
 const config = {
   API_SECRET: "V36xZnA84pk0",
   API_KEY: "MyTsel_webview01",
-  API_URL: "https://insurtech.klop.co/v1/app-api",
+  API_URL: "https://insurtech.klop.co/app/v1/app-api/",
 };
 
 export default config;
